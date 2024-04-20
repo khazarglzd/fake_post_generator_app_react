cf0eeac01f7eafffc91a54bd13f2fd974f3905b9
khazarglzd <khazarguluzade@gmail.com>
2024-04-20 16:05:45 +0400

    adding scss & styling tweet box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './style.css';
+import './style.scss';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="tweet">
+      <div className='tweet-author'>
+        <img alt="Avatar resmi" src="https://yt3.ggpht.com/tdohfuP8L3-gTrlMeC4s2k2sbByUJEyXXsZ0CaiL2nyMqVdmm20ZlDo5uCkdDxDdN1MNZJxDrLU=s88-c-k-c0x00ffffff-no-rj" />
+        <div>
+          <div className='name'>
+            Khazar Guluzade
+          </div>
+          <div className='username'>
+            @khazarglzd
+          </div>
+        </div>
+      </div>
+      <div className='tweet-content'>
+        <p> This is a fake tweet written for the test. </p>
+      </div>
     </div>
   );
 }
