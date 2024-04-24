d382661a6174437d262d1c61d6e95c0ca2c6411b
khazarglzd <khazarguluzade@gmail.com>
2024-04-24 16:57:16 +0400

    adding avatar loader component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './style.scss';
 import { ReplyIcon, RetweetIcon, LikeIcon, ShareIcon, VerifiedIcon } from './icons';
 import { useState } from "react"
+import { AvatarLoader } from './loaders';
 
 function App() {
 
@@ -8,12 +9,12 @@ function App() {
   const [username, setUsername] = useState();
   const [isVerified, setIsVerified] = useState();
   const [tweet, setTweet] = useState(
-    "This is the deafult tweet written by @khazarglzd and you can find him at https://github.com/khazarglzd github adress."
+    "This is the default tweet written by @khazarglzd and you can find him at https://github.com/khazarglzd github adress."
   );
   const [avatar, setAvatar] = useState();
-  const [retweets, setRetweets] = useState(0);
-  const [quoteTweets, setQuoteTweets] = useState(0);
-  const [likes, setLikes] = useState(0);
+  const [retweets, setRetweets] = useState();
+  const [quoteTweets, setQuoteTweets] = useState();
+  const [likes, setLikes] = useState();
 
   const tweetFormat = tweet => {
     tweet = tweet
@@ -21,6 +22,15 @@ function App() {
       .replace(/#([\wşçöğüıİəƏ]+)/gi, '<span>#$1</span>')
       .replace(/(https?:\/\/[\w\.\/]+)/, '<span>$1</span>')
     return tweet;
+  };
+
+  const avatarHandle = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', function () {
+      setAvatar(this.result);
+    });
+    reader.readAsDataURL(file);
   };
 
   const formatNumber = number => {
@@ -48,6 +58,7 @@ function App() {
             <input
               type="text"
               className="input"
+              placeholder='Name Surname'
               value={name}
               onChange={e => setName(e.target.value)}
             />
@@ -57,6 +68,7 @@ function App() {
             <input
               type="text"
               className="input"
+              placeholder='Username'
               value={username}
               onChange={e => setUsername(e.target.value)}
             />
@@ -71,12 +83,16 @@ function App() {
             />
           </li>
           <li>
+            <label>Avatar</label>
+            <input type="file" className="input" onChange={avatarHandle} />
+          </li>
+          <li>
             <label>Total Retweets:</label>
             <input
               type="number"
               className="input"
               value={retweets}
-              onClick={e => setRetweets("")}
+              placeholder={0}
               onChange={e => setRetweets(e.target.value)}
             />
           </li>
@@ -86,7 +102,7 @@ function App() {
               type="number"
               className="input"
               value={quoteTweets}
-              onClick={e => setQuoteTweets("")}
+              placeholder={0}
               onChange={e => setQuoteTweets(e.target.value)}
             />
           </li>
@@ -96,7 +112,7 @@ function App() {
               type="number"
               className="input"
               value={likes}
-              onClick={e => setLikes("")}
+              placeholder={0}
               onChange={e => setLikes(e.target.value)}
             />
           </li>
@@ -106,7 +122,7 @@ function App() {
       <div className='tweet-container'>
         <div className="tweet">
           <div className='tweet-author'>
-            <img alt="Avatar resmi" src="https://yt3.ggpht.com/tdohfuP8L3-gTrlMeC4s2k2sbByUJEyXXsZ0CaiL2nyMqVdmm20ZlDo5uCkdDxDdN1MNZJxDrLU=s88-c-k-c0x00ffffff-no-rj" />
+            {(avatar && <img src={avatar} />) || <AvatarLoader />}
             <div>
               <div className='name'>
                 {name || "Name Surname"}
